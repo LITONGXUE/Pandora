a3a894471965e7364cc055a5e784dab8d847e110
李文杨 <liwenyang@172-15-69-207.lightspeed.austtx.sbcglobal.net>
2020-04-15 20:11:06 +0800

    add file

---
+++ src/components/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  props: {
+    msg: String
+  },
+  mounted(){
+      
+  },
+  methods:{
+      getViewJson(){
+          this.$axios.post('http://mock.in.songguo7.com/mock/5e816dc7246e9c1ac04b733a/pandora/viewList')
+          .then(({data})=>{
+              let prams = data.view;
+              console.log(prams)
+          })
+      }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.home{
+
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
-
-Vue.config.productionTip = false
-
+import axios from 'axios';
+Vue.config.productionTip = false;
+Vue.prototype.$axios = axios;
 new Vue({
   render: h => h(App),
 }).$mount('#app')
